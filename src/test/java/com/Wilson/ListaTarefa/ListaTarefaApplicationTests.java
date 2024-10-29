879c854fc9a67db7101a966aac0e4848ce6e44bc
WilsonQdop <wilson.qdop@gmail.com>
2024-10-29 01:58:52 -0300

    Conectando banco de dados

---
+++ src/test/java/com/Wilson/ListaTarefa/ListaTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wilson.ListaTarefa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
